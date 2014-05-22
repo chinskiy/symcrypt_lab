2f4fb0b84c2b54f819a2daff6edf714a4cdf7214
chinskiy <chinskiy93@gmail.com>
2014-05-22 13:05:53 +0300

    now working lab. fix bugs

--- lab4/cryptoanaliz.py
+++ lab4/cryptoanaliz.py
@@ -5,6 +5,15 @@ def findrstat(x, z):
     return r
 
 
+def check(seq, code):
+    chk = 1
+    for _ in range(len(seq)):
+        if seq[_] != code[_]:
+            chk = 0
+            break
+    return chk
+
+
 def nextvect(arr):
     arr = arr[::-1]
     c_b = 1
@@ -16,13 +25,29 @@ def nextvect(arr):
 
 def get_candidates(lfrs, arr, numb, c):
     cand = []
-    for _ in range(2 ** len(arr) - 1):
-        lfrs.set_state(arr)
+    qq = []
+    for _ in range(2 ** len(arr)):
+        lfrs.set_state(arr[:])
         seq = lfrs.gener_sequence(numb)
-        if findrstat(seq, code) > c:
-            cand.append(seq)
+        qq.append(findrstat(seq, code))
+        if findrstat(seq, code) < c:
+            cand.append(arr)
         arr = nextvect(arr)
+    print(min(qq))
     return cand
+
+
+def get_candidates3(cand1, cand2, g1, arr):
+    cand3 = []
+    for i in range(len(cand1)):
+        for j in range(len(cand2)):
+            for k in range(2 ** len(arr)):
+                g1.set_state(cand1[i][:], cand2[j][:], arr[:])
+                seq = g1.gen_sequence(50)
+                if check(seq, code) == 1:
+                    cand3.append(arr)
+                arr = nextvect(arr)
+    return cand3
 
 
 strcode = '100001111110111101001001111001001110001101011001100101101000110000111011001' \
@@ -32,4 +57,6 @@ strcode = '100001111110111101001001111001001110001101011001100101101000110000111
           '010110001010001001110101111011110011011111011001100001110110011000101011100' \
           '010000001000110011100001011011101001010011011100100001010001100011001101111' \
           '00010001100000010110111010010111011011111011000000'
+
+
 code = [int(code) for code in strcode]
--- lab4/lfrs.py
+++ lab4/lfrs.py
@@ -1,15 +1,15 @@
 class LFSR():
     def __init__(self, polyn):
         """"(list(int)) -> None"""
-        self.polyn = polyn[::-1][1:len(polyn)]
+        self.polyn = polyn[1:len(polyn)][::-1]
 
     def step(self):
         """(None) -> bool"""
         numb = 0
         for _ in range(len(self.polyn)):
             numb ^= self.polyn[_] & self.state[_]
-        self.state.insert(0, numb)
-        return self.state.pop()
+        self.state.append(numb)
+        return self.state.pop(0)
 
     def gener_sequence(self, length):
         """(int) -> list(int)"""
@@ -33,7 +33,11 @@ class GeffeGen():
         x, y, s = self.l1.step(), self.l2.step(), self.l3.step()
         return s & x ^ (1 ^ s) & y
 
-    def gener_sequence(self, length):
+    def set_state(self, st1, st2, st3):
+        """(list(int), list(int), list(int)) -> None"""
+        self.l1.state, self.l2.state, self.l3.state = st1, st2, st3
+
+    def gen_sequence(self, length):
         """(int) -> list(int)"""
         result = []
         for _ in range(length):
