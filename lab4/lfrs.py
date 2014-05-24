40b7e6c31077896093fd08f9d98837d287a1736e
chinskiy <chinskiy93@gmail.com>
2014-05-24 17:02:06 +0300

    update output

--- lab4/lfrs.py
+++ lab4/lfrs.py
@@ -1,10 +1,8 @@
 class LFSR():
     def __init__(self, polyn):
-        """"(list(int)) -> None"""
         self.polyn = polyn[1:len(polyn)][::-1]
 
     def step(self):
-        """(None) -> bool"""
         numb = 0
         for _ in range(len(self.polyn)):
             numb ^= self.polyn[_] & self.state[_]
@@ -12,28 +10,22 @@ class LFSR():
         return self.state.pop(0)
 
     def gener_sequence(self, length):
-        """(int) -> list(int)"""
         return [self.step() for _ in range(length)]
 
     def set_state(self, state):
-        """(list(int)) -> None"""
         self.state = state
 
 
 class GeffeGen():
     def __init__(self, l1, l2, l3):
-        """(LFSR, LFSR, LFSR) -> None"""
         self.l1, self.l2, self.l3 = l1, l2, l3
 
     def step(self):
-        """(None) -> bool"""
         x, y, s = self.l1.step(), self.l2.step(), self.l3.step()
         return s & x ^ (1 ^ s) & y
 
     def set_state(self, st1, st2, st3):
-        """(list(int), list(int), list(int)) -> None"""
         self.l1.state, self.l2.state, self.l3.state = st1, st2, st3
 
     def gen_sequence(self, length):
-        """(int) -> list(int)"""
         return [self.step() for _ in range(length)]
--- lab4/main.py
+++ lab4/main.py
@@ -8,11 +8,11 @@ if __name__ == '__main__':
     l2 = LFSR([1, 0, 0, 0, 0, 1, 1, 0, 1, 1])
     l3 = LFSR([1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1])
     g1 = GeffeGen(l1, l2, l3)
-    n1, n2, c1, c2 = 106, 82, 36, 30
+    n1, n2, c1, c2 = 112, 95, 38, 33
     cand1 = get_candidates(l1, [0 for _ in range(len(l1.polyn))], n1, c1)
     cand2 = get_candidates(l2, [0 for _ in range(len(l2.polyn))], n2, c2)
     res = get_res(cand1, cand2, g1, [0 for _ in range(len(l3.polyn))])
     for _ in range(len(res)):
         print('l', _ + 1, ' = ', sep='', end='')
-        print(res[_])
+        print(''.join(str(_) for _ in res[_]))
     print(time.time() - t1)
